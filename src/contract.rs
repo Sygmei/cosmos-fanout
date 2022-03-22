671cf0f34f88b9373078c4769a0a0353f03a38ca
Sygmei <3835355+Sygmei@users.noreply.github.com>
2022-03-22 06:58:58 +0100

    [chore] clearn + queries

--- src/contract.rs
+++ src/contract.rs
@@ -1,14 +1,14 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    coins, to_binary, BankMsg, Binary, Coin, Deps, DepsMut, Empty, Env, MessageInfo, Response,
-    StdResult, Uint128,
+    coins, to_binary, Addr, BankMsg, Binary, Coin, Deps, DepsMut, Empty, Env, MessageInfo,
+    Response, StdResult, Storage, Uint128,
 };
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-use crate::state::{State, BENEFICIARIES, STATE};
+use crate::msg::{ExecuteMsg, InstantiateMsg, PotDonorResponse, QueryMsg};
+use crate::state::{State, BENEFICIARIES, DONORS, STATE};
 
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cosmos-fanout";
@@ -39,6 +39,8 @@ pub fn execute(
     info: MessageInfo,
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
+    // Acts like a message dispatcher
+    // Will reroute the message to the correct handler
     match msg {
         ExecuteMsg::RegisterBeneficiary {} => register_beneficiary(deps, info),
         ExecuteMsg::AddToPot {} => add_to_pot(deps, info),
@@ -76,39 +78,68 @@ fn split_coins_into_parts(coins: &Vec<Coin>, parts: u32) -> Vec<Vec<Coin>> {
     return split_coins;
 }
 
+fn register_donation_infos(
+    storage: &mut dyn Storage,
+    donor_addr: Addr,
+    mut donation_funds: Vec<Coin>,
+) {
+    let _ = DONORS.update(storage, donor_addr, |donor| -> Result<_, ContractError> {
+        if let Some(mut donor_funds) = donor {
+            donor_funds.append(&mut donation_funds);
+            Ok(donor_funds)
+        } else {
+            Ok(donation_funds)
+        }
+    });
+}
+
+fn register_beneficiary_donation_infos(
+    storage: &mut dyn Storage,
+    beneficiary_addr: Addr,
+    mut donation_funds: Vec<Coin>,
+) {
+    let _ = BENEFICIARIES.update(
+        storage,
+        beneficiary_addr,
+        |beneficiary_funds| -> Result<_, ContractError> {
+            if let Some(mut beneficiary_funds) = beneficiary_funds {
+                beneficiary_funds.append(&mut donation_funds);
+                Ok(beneficiary_funds)
+            } else {
+                Ok(donation_funds)
+            }
+        },
+    );
+}
+
 pub fn add_to_pot(deps: DepsMut, info: MessageInfo) -> Result<Response, ContractError> {
     let mut amount_of_beneficiaries = 0;
     for _beneficiary in BENEFICIARIES.keys(deps.storage, None, None, cosmwasm_std::Order::Ascending)
     {
         amount_of_beneficiaries += 1;
     }
+    register_donation_infos(deps.storage, info.sender.clone(), info.funds.clone());
     let funds_for_each = split_coins_into_parts(&info.funds, amount_of_beneficiaries);
-    // TODO: Add DONORS informations
-    /*DONORS.update(
-        deps.storage,
-        info.sender,
-        |donor| -> Result<_, ContractError> {
-            // TODO: Extend vector with funds
-            Ok(info.funds)
-        },
-    );*/
 
+    // Building a new Response
     let mut response: Response<Empty> = Response::new();
 
-    let mut beneficiaries_out: Vec<String> = Vec::new();
-    // TODO: Add beneficiary stuff
-    for (beneficiary, coin_part) in BENEFICIARIES
-        .keys(deps.storage, None, None, cosmwasm_std::Order::Ascending)
-        .zip(funds_for_each)
+    // Distributing money amongst beneficiaries
+    let mut beneficiaries_list: Vec<Addr> = Vec::new();
+    for beneficiary in BENEFICIARIES.keys(deps.storage, None, None, cosmwasm_std::Order::Ascending)
     {
-        let beneficiary_addr = beneficiary.expect("invalid beneficiary address");
-        beneficiaries_out.push(beneficiary_addr.clone().into_string());
+        beneficiaries_list.push(beneficiary?);
+    }
+    for (beneficiary, coin_part) in beneficiaries_list.iter().zip(funds_for_each) {
+        // We are adding a new "BankMsg" for each beneficiary
         response = response.add_message(BankMsg::Send {
             amount: coins(coin_part[0].amount.u128(), coin_part[0].denom.clone()),
-            to_address: beneficiary_addr.clone().into_string(),
+            to_address: beneficiary.clone().into_string(),
         });
+        register_beneficiary_donation_infos(deps.storage, beneficiary.clone(), coin_part);
     }
-    let beneficiaries_as_str = format!("{:?}", beneficiaries_out);
+
+    let beneficiaries_as_str = format!("{:?}", beneficiaries_list);
     response = response.add_attribute("beneficiaries", beneficiaries_as_str);
     response = response.add_attribute(
         "amount_of_beneficiaries",
@@ -128,105 +159,22 @@ pub fn admin_action(deps: DepsMut, info: MessageInfo) -> Result<Response, Contra
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
-pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
-    /*match msg {
-        QueryMsg::GetTotal {} => to_binary(&query_total(deps)?),
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
         QueryMsg::GetDonor { donor } => to_binary(&query_donor(deps, donor)?),
-    }*/
-    to_binary(&0)
-}
-
-/*fn query_donor(deps: Deps, donor: Addr) -> StdResult<PotDonorResponse> {
-    let state = STATE.load(deps.storage)?;
-    let total = Uint128::new(0);
-    if let Some(donor_coins) = state.donors.get(&donor) {
-        for donor_coin in donor_coins {
-            total.saturating_add(donor_coin.amount);
-        }
-    }
-
-    Ok(PotDonorResponse { total: total })
-}
-
-fn query_total(deps: Deps) -> StdResult<PotTotalResponse> {
-    let state = STATE.load(deps.storage)?;
-    let total = Uint128::new(0);
-    for donor_coins in state.donors.values() {
-        for donor_coin in donor_coins {
-            total.saturating_add(donor_coin.amount);
-        }
-    }
-    Ok(PotTotalResponse { total: total })
-}*/
-
-/*#[cfg(test)]
-mod tests {
-    use super::*;
-    use cosmwasm_std::testing::{mock_dependencies_with_balance, mock_env, mock_info};
-    use cosmwasm_std::{coins, from_binary};
-
-    #[test]
-    fn proper_initialization() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(1000, "earth"));
-
-        // we can just call .unwrap() to assert this was a success
-        let res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-        assert_eq!(0, res.messages.len());
-
-        // it worked, let's query the state
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: PotTotalResponse = from_binary(&res).unwrap();
-        assert_eq!(17, value.count);
-    }
-
-    #[test]
-    fn increment() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Increment {};
-        let _res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // should increase counter by 1
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: PotTotalResponse = from_binary(&res).unwrap();
-        assert_eq!(18, value.count);
-    }
-
-    #[test]
-    fn reset() {
-        let mut deps = mock_dependencies_with_balance(&coins(2, "token"));
-
-        let msg = InstantiateMsg { count: 17 };
-        let info = mock_info("creator", &coins(2, "token"));
-        let _res = instantiate(deps.as_mut(), mock_env(), info, msg).unwrap();
-
-        // beneficiary can release it
-        let unauth_info = mock_info("anyone", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let res = execute(deps.as_mut(), mock_env(), unauth_info, msg);
-        match res {
-            Err(ContractError::Unauthorized {}) => {}
-            _ => panic!("Must return unauthorized error"),
-        }
-
-        // only the original creator can reset the counter
-        let auth_info = mock_info("creator", &coins(2, "token"));
-        let msg = ExecuteMsg::Reset { count: 5 };
-        let _res = execute(deps.as_mut(), mock_env(), auth_info, msg).unwrap();
-
-        // should now be 5
-        let res = query(deps.as_ref(), mock_env(), QueryMsg::GetCount {}).unwrap();
-        let value: PotTotalResponse = from_binary(&res).unwrap();
-        assert_eq!(5, value.count);
     }
 }
-*/
+
+fn query_donor(deps: Deps, donor: Addr) -> StdResult<PotDonorResponse> {
+    if let Ok(donor_infos) = DONORS.load(deps.storage, donor.clone()) {
+        return Ok(PotDonorResponse {
+            donor: donor,
+            donations: donor_infos,
+        });
+    }
+
+    return Ok(PotDonorResponse {
+        donor: donor,
+        donations: [].to_vec(),
+    });
+}
--- src/msg.rs
+++ src/msg.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Addr, Uint128};
+use cosmwasm_std::{Addr, Coin};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -15,18 +15,11 @@ pub enum ExecuteMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    GetTotal {},
     GetDonor { donor: Addr },
-}
-
-// We define a custom struct for each query response
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct PotTotalResponse {
-    pub total: Uint128,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct PotDonorResponse {
-    pub total: Uint128,
+    pub donor: Addr,
+    pub donations: Vec<Coin>,
 }
